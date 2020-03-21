ad85f2f55a385102f44c1c09cd68ef1e6d5808d1
pierrettebertrand <pierrette.bertrand@gmail.com>
2020-03-21 19:05:30 +0100

    resolve user by gps_coordinates

--- common/models/need-help.js
+++ common/models/need-help.js
@@ -1,5 +1,38 @@
 'use strict';
 
-module.exports = function(Needhelp) {
+module.exports = function (Needhelp) {
+  Needhelp.matching = function (id, maxDistance, cb) {
+    Needhelp.findOne({
+      where: {
+        id,
+      },
+    }, (err, needer) => {
+      Needhelp.app.models.Helper.find({
+        where: {
+          gps_coordinates: {
+            near: needer.gps_coordinates,
+            maxDistance,
+            unit: 'meters',
+          },
+        },
+      }, (err, value) => cb(null, value));
+    });
+  };
 
+  Needhelp.remoteMethod('matching', {
+    accepts: [{
+      arg: 'id',
+      type: 'string',
+    }, {
+      arg: 'maxDistance',
+      type: 'number',
+    }],
+    returns: {
+      args: 'response',
+      type: 'object',
+    },
+    http: {
+      verb: 'get',
+    },
+  });
 };
