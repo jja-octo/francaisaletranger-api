c4015eac984d4a20b6ec768b3d7fa8593b2ca2a6
pierrettebertrand <pierrette.bertrand@gmail.com>
2020-03-22 10:01:49 +0100

    improve query to database and clean output

--- common/models/need-help.js
+++ common/models/need-help.js
@@ -1,4 +1,5 @@
 'use strict';
+var loopback = require('loopback');
 
 module.exports = function (Needhelp) {
   Needhelp.matching = function (id, maxDistance, cb) {
@@ -9,13 +10,40 @@ module.exports = function (Needhelp) {
     }, (err, needer) => {
       Needhelp.app.models.Helper.find({
         where: {
-          gps_coordinates: {
-            near: needer.gps_coordinates,
-            maxDistance,
-            unit: 'meters',
-          },
+          and: [{
+            gps_coordinates: {
+              near: needer.gps_coordinates,
+              maxDistance,
+              unit: 'meters',
+            },
+          }, {
+            nombre_hebergement: {
+              gte: needer.nombre_hebergement,
+            },
+          }, {
+            approvisionnement: needer.approvisionnement,
+          }, {
+            autres: needer.autres,
+          }],
         },
-      }, (err, value) => cb(null, value));
+      }, (err, foundHelperList) => {
+        const preparedHelperList = foundHelperList.map((foundHelper) => {
+          const neederLocation = new loopback.GeoPoint(needer.gps_coordinates);
+          var helperLocation = new loopback.GeoPoint(foundHelper.gps_coordinates);
+          return {
+            id: foundHelper.id,
+            nom: foundHelper.nom,
+            prenom: foundHelper.prenom,
+            nombre_hebergement: foundHelper.nombre_hebergement,
+            approvisionnement: foundHelper.approvisionnement,
+            autres: foundHelper.autres,
+            distanceInMeters: neederLocation.distanceTo(helperLocation, {
+              type: 'meters',
+            }),
+          };
+        });
+        cb(err, preparedHelperList);
+      });
     });
   };
 
