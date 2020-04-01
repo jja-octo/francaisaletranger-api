1438d20d04b15ea6e5ba931335cf6de7cf7be814
Julien Jakubowski <jjakubowski@octo.com>
2020-04-01 22:39:51 +0200

    refacto and test harness

---
+++ tests/unit/common/models/need-help.test.js
@@ -0,0 +1,129 @@
+'use strict';
+const modelFn = require('../../../../common/models/need-help')
+const expect = require('expect.js');
+
+function needHelpModelStub(needHelp, helpersQueryData) {
+  const needHelpTable = {
+    neederId: needHelp
+  }
+  const sqlQueryData = helpersQueryData.map((h) => {return {id: h.id}})
+  const NeedhelpStub = {
+    observe: () => {
+    },
+    remoteMethod: () => {
+    },
+    findOne: (criteria, callback) => {
+      callback(null,needHelpTable[criteria.where.id]);
+    },
+    dataSource: {
+      connector: {
+        query: (sqlStatement, sqlParams, callback) => {callback(null,sqlQueryData)}
+      }
+    },
+    app: {
+      models: {
+        Helper: {
+          find: (criteria, callback) => {callback(null,helpersQueryData)}
+        }
+      }
+    }
+  }
+  modelFn(NeedhelpStub);
+  return NeedhelpStub  
+}
+
+function comparableResults(results) {
+  return results.map((r) => {
+    return {
+      id: r.id,
+      distanceInMeters: Math.round(r.distanceInMeters),
+      criteresMatching: r.criteresMatching
+    }
+  })
+}
+describe('Needhelp (unit)', () => {
+  describe('matching()', () => {
+    it('navigates into legacy code', (done) => {
+      // Given
+
+      const needer = {
+        id: 'neederId',
+        gps_coordinates: {
+          lng: 2.9,
+          lat: 50
+        },
+        nombre_hebergement: 2,
+        approvisionnement: true,
+        conseils: true,
+        autres: false,
+      }
+
+      const foundHelpers = [{
+        id: 'helper_1',
+        nom: 'Smith',
+        prenom: 'John',
+        nombre_hebergement: 2,
+        approvisionnement: true,
+        conseils: true,
+        autres: false,
+        gps_coordinates: {
+          lng: 2.9,
+          lat: 50
+        }
+      },
+      {
+        id: 'helper_2',
+        nom: 'Smith',
+        prenom: 'Julie',
+        nombre_hebergement: 2,
+        approvisionnement: false,
+        conseils: false,
+        autres: true,
+        gps_coordinates: {
+          lng: 2.9,
+          lat: 50
+        }
+      },
+      {
+        id: 'helper_3',
+        nom: 'Smith',
+        prenom: 'John',
+        nombre_hebergement: 2,
+        approvisionnement: true,
+        conseils: true,
+        autres: false,
+        gps_coordinates: {
+          lng: 3,
+          lat: 50
+        }
+      }]
+      const maxDistance = 42
+
+      // When
+      needHelpModelStub(needer,foundHelpers).matching(needer.id, maxDistance, (err, results) => {
+
+        // Then
+        expect(comparableResults(results)).to.eql(
+          [
+            {
+              id: 'helper_1',
+              distanceInMeters: 0,
+              criteresMatching: { score: 3, total: 3 }
+            },
+            {
+              id: 'helper_3',
+              distanceInMeters: 7147,
+              criteresMatching: { score: 3, total: 3 }
+            },
+            {
+              id: 'helper_2',
+              distanceInMeters: 0,
+              criteresMatching: { score: 1, total: 3 }
+            }
+          ]
+        )
+        done()
+      })
+    });
+  });
+});
